486ef3a358e8b14b16d6fb4a158e5ce0a8261b20
Sergey <you@example.com>
2019-03-01 21:36:17 +0300

    add config deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ set -e
 zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
-     -H "Authorization: Bearer $NETLIFY_KEY" \
+     -H "Authorization: Bearer $POCKET_KEY" \
      --data-binary "@build.zip" \
      https://api.netlify.com/api/v1/sites
